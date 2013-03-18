04a3166c518c30bd42ba9f4e84bff01a2432686c
cottin_huynh <mini.spam8888@googlemail.com>
2013-03-18 17:52:05 +0100

    Fehler behoben

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/PropertyReader.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/PropertyReader.java
@@ -1,21 +1,22 @@
 package de.codecentric.psd.worblehat.web.controller;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 
 public class PropertyReader {
 
 	public static String getVersion() throws FileNotFoundException, IOException {
-		Properties properties = new Properties();
+		/*
+		 * Properties properties = new Properties();
+		 * 
+		 * properties.load(new FileInputStream(
+		 * "../resources/application.properties"));
+		 * System.out.println("Version = " +
+		 * properties.getProperty("application.version")); return
+		 * properties.getProperty("application.version");
+		 */
 
-		properties.load(new FileInputStream(
-				"../resources/application.properties"));
-		System.out.println("Version = "
-				+ properties.getProperty("application.version"));
-		return properties.getProperty("application.version");
-
+		return null;
 	}
 
 }
