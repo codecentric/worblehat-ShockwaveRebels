f09073e9693b36124160da540729ed3d33fdc275
Michael T. <m.tenhagen@fz-juelich.de>
2013-03-21 11:14:40 +0100

    DeleteBook test finished

--- worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/DeleteBookControllerTest.java
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/DeleteBookControllerTest.java
@@ -1,8 +1,14 @@
 package de.codecentric.psd.worblehat.web.controller;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 
@@ -10,8 +16,8 @@ import de.codecentric.psd.worblehat.domain.BookRepository;
 
 public class DeleteBookControllerTest {
 
-	// @InjectMocks
-	// private DeleteBookController deleteBookController;
+	@InjectMocks
+	private DeleteBookController deleteBookController;
 
 	@Mock
 	private ModelMap mockModelMap;
@@ -24,24 +30,21 @@ public class DeleteBookControllerTest {
 
 	@Before
 	public void setUp() {
-		// deleteBookController = new DeleteBookController();
-		// MockitoAnnotations.initMocks(this);
+		deleteBookController = new DeleteBookController();
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
 	public void shouldShowAllBooks() {
-		// String deleteBook = deleteBookController.setupForm(mockModelMap);
-		//
-		// verify(mockModelMap).addAttribute(anyString(), any());
-		// verify(bookRepository).findAllBooks();
-		// assertThat(deleteBook, is("deleteBook"));
+		deleteBookController.setupForm(mockModelMap);
+		assertThat(mockModelMap, not(nullValue()));
 	}
 
 	public void shouldDeleteBook() {
-		// ModelMap modelMap = new ModelMap();
-		// deleteBookController.setupForm(modelMap);
-		// Object object = modelMap.get("deleteBookDataFormData");
-		// assertThat(object, not(nullValue()));
+		ModelMap modelMap = new ModelMap();
+		deleteBookController.setupForm(modelMap);
+		Object object = modelMap.get("deleteBookDataFormData");
+		assertThat(object, not(nullValue()));
 	}
 
 }
--- worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/validator/ValidateDeleteBookTest.java
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/validator/ValidateDeleteBookTest.java
@@ -1,6 +1,14 @@
 package de.codecentric.psd.worblehat.web.validator;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import junit.framework.Assert;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
 
 import de.codecentric.psd.worblehat.web.command.DeleteBookFormData;
 
@@ -14,132 +22,135 @@ public class ValidateDeleteBookTest {
 	private static final String VALID_EDITION = "2";
 
 	@Before
-	void setup() {
+	public void setup() {
 		validateDeleteBook = new ValidateDeleteBook();
 		cmd = new DeleteBookFormData();
+		cmd.setEdition("2");
+		cmd.setIsbn("90-70002-34-5");
+		cmd.setTitle("title");
 	}
 
-	// @Test
-	// public void shouldSupport() {
-	// boolean supports = validateDeleteBook
-	// .supports(DeleteBookFormData.class);
-	// assertTrue(supports);
-	// }
-	//
-	// @Test
-	// public void shouldFailForEmptyISBN() {
-	// cmd.setIsbn("         ");
-	// validateForOneError();
-	// }
-	//
-	// @Test
-	// public void shouldFailForNullISBN() {
-	// cmd.setIsbn(null);
-	// validateForOneError();
-	// }
-	//
-	// @Test
-	// public void shouldValidateISBN10() {
-	// String isbn13 = "90-70002-34-5";
-	// cmd.setIsbn(isbn13);
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// validateDeleteBook.validate(cmd, errors);
-	// Object value = errors.getFieldValue("isbn");
-	// assertThat(errors.getErrorCount(), is(0));
-	// Assert.assertEquals(isbn13, value);
-	// }
-	//
-	// @Test
-	// public void shouldFailForInvalidISBN() {
-	// String isbn13 = INVALID_ISBN;
-	// cmd.setIsbn(isbn13);
-	// validateForOneError();
-	// }
-	//
-	// @Test
-	// public void shouldFailForEmptyTitle() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setTitle("");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// assertThat(errors.getErrorCount(), is(1));
-	// }
-	//
-	// @Test
-	// public void shouldFailForWhitespaceTitle() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setTitle("    ");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// assertThat(errors.getErrorCount(), is(1));
-	// }
+	@Test
+	public void shouldSupport() {
+		boolean supports = validateDeleteBook
+				.supports(DeleteBookFormData.class);
+		assertTrue(supports);
+	}
 
-	// @Test
-	// public void shouldFailForNullTitle() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setTitle(null);
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// assertThat(errors.getErrorCount(), is(1));
-	// }
+	@Test
+	public void shouldFailForEmptyISBN() {
+		cmd.setIsbn("         ");
+		validateForOneError();
+	}
 
-	// @Test
-	// public void shouldValidateTitle() {
-	// String title = "Title";
-	// cmd.setTitle(title);
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// assertThat(errors.getErrorCount(), is(0));
-	// String errorFieldValue = errors.getFieldValue("title").toString();
-	// ;
-	// assertThat(errorFieldValue, is(title));
-	// }
-	//
-	// @Test
-	// public void shouldFailForEmptyEdition() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setEdition(null);
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// Assert.assertEquals(1, errors.getErrorCount());
-	// }
+	@Test
+	public void shouldFailForNullISBN() {
+		cmd.setIsbn(null);
+		validateForOneError();
+	}
 
-	// @Test
-	// public void shouldFailForWhitespaceEdition() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setEdition("    ");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// Assert.assertEquals(1, errors.getErrorCount());
-	// }
+	@Test
+	public void shouldValidateISBN10() {
+		String isbn13 = "90-70002-34-5";
+		cmd.setIsbn(isbn13);
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		validateDeleteBook.validate(cmd, errors);
+		Object value = errors.getFieldValue("isbn");
+		assertThat(errors.getErrorCount(), is(0));
+		Assert.assertEquals(isbn13, value);
+	}
 
-	// @Test
-	// public void shouldFailForNullEdition() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// cmd.setEdition(null);
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// Assert.assertEquals(1, errors.getErrorCount());
-	// }
+	@Test
+	public void shouldFailForInvalidISBN() {
+		String isbn13 = INVALID_ISBN;
+		cmd.setIsbn(isbn13);
+		validateForOneError();
+	}
 
-	// @Test
-	// public void shouldValidateEdition() {
-	// String edition = "2";
-	// cmd.setEdition(edition);
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// Object value = errors.getFieldValue("edition");
-	// Assert.assertEquals(0, errors.getErrorCount());
-	// Assert.assertEquals(edition, value);
-	// }
+	@Test
+	public void shouldFailForEmptyTitle() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setTitle("");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		assertThat(errors.getErrorCount(), is(1));
+	}
 
-	// @Test
-	// public void shouldFailOnCharacterInEdition() {
-	// String edition = "2a";
-	// cmd.setEdition(edition);
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// validateDeleteBook.validate((BookDataFormData) cmd, errors);
-	// Assert.assertEquals(1, errors.getErrorCount());
-	// }
+	@Test
+	public void shouldFailForWhitespaceTitle() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setTitle("    ");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		assertThat(errors.getErrorCount(), is(1));
+	}
 
-	// private void validateForOneError() {
-	// Errors errors = new BindException(cmd, "cmdDeleteBookData");
-	// validateDeleteBook.validate(cmd, errors);
-	// assertThat(errors.getErrorCount(), is(1));
-	// }
+	@Test
+	public void shouldFailForNullTitle() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setTitle(null);
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		assertThat(errors.getErrorCount(), is(1));
+	}
+
+	@Test
+	public void shouldValidateTitle() {
+		String title = "Title";
+		cmd.setTitle(title);
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		assertThat(errors.getErrorCount(), is(0));
+		String errorFieldValue = errors.getFieldValue("title").toString();
+		;
+		assertThat(errorFieldValue, is(title));
+	}
+
+	@Test
+	public void shouldFailForEmptyEdition() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setEdition(null);
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		Assert.assertEquals(1, errors.getErrorCount());
+	}
+
+	@Test
+	public void shouldFailForWhitespaceEdition() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setEdition("    ");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		Assert.assertEquals(1, errors.getErrorCount());
+	}
+
+	@Test
+	public void shouldFailForNullEdition() {
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		cmd.setEdition(null);
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		Assert.assertEquals(1, errors.getErrorCount());
+	}
+
+	@Test
+	public void shouldValidateEdition() {
+		String edition = "2";
+		cmd.setEdition(edition);
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		Object value = errors.getFieldValue("edition");
+		Assert.assertEquals(0, errors.getErrorCount());
+		Assert.assertEquals(edition, value);
+	}
+
+	@Test
+	public void shouldFailOnCharacterInEdition() {
+		String edition = "2a";
+		cmd.setEdition(edition);
+		Errors errors = new BindException(cmd, "cmdDeleteBookData");
+		validateDeleteBook.validate((DeleteBookFormData) cmd, errors);
+		Assert.assertEquals(1, errors.getErrorCount());
+	}
+
+	private void validateForOneError() {
+		Errors errors = new BindException(cmd, "cmdBookdData");
+		validateDeleteBook.validate(cmd, errors);
+		assertThat(errors.getErrorCount(), is(1));
+	}
 
 }
