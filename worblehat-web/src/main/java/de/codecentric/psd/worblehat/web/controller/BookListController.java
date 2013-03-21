64e261cc18751aa1cd4171db83735297f5f30569
cottin_huynh <mini.spam8888@googlemail.com>
2013-03-21 19:00:40 +0100

    removed output

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/BookListController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/BookListController.java
@@ -37,8 +37,7 @@ public class BookListController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String setupForm(ModelMap modelMap) {
 		List<Book> books = bookRepository.findAllBooks();
-		for (Book b : books)
-			System.err.println(b.getDescription());
+
 		modelMap.addAttribute("books", books);
 		return "bookList";
 	}
